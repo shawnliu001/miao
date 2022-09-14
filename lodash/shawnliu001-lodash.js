f7b68472fcfca518e85cb707d91003525b6b8adc
shaawn <2899781009@qq.com>
2022-09-14 19:10:20 +0800

    chunk

--- lodash/shawnliu001-lodash.js
+++ lodash/shawnliu001-lodash.js
@@ -1,3 +1,34 @@
 var shawnliu001 = {
-  chunk: function (arr, size) {},
+  chunk: function (arr, size) {
+    if (size === 1 || size >= arr.length) return [arr];
+    if (arr.length % size === 0) {
+      let subArrNumber = arr.length / size;
+      let containerArr = new Array(subArrNumber).fill(null).map((el) => []);
+      let newRoundIndex = 0;
+      for (let i = 0; i < containerArr.length; i++) {
+        for (let j = newRoundIndex; j < arr.length; j++) {
+          containerArr[i].push(arr[j]);
+          if (containerArr[i].length === size) {
+            newRoundIndex += size - 1;
+            break;
+          }
+        }
+      }
+      return containerArr;
+    } else {
+      let subArrNumber = (arr.length - (arr.length % size)) / size;
+      let containerArr = new Array(subArrNumber + 1).fill(null).map((el) => []);
+      let newRoundIndex = 0;
+      for (let i = 0; i < containerArr.length; i++) {
+        for (let j = newRoundIndex; j < arr.length; j++) {
+          containerArr[i].push(arr[j]);
+          if (containerArr[i].length === size) {
+            newRoundIndex += size - 1;
+            break;
+          }
+        }
+      }
+      return containerArr;
+    }
+  },
 };
